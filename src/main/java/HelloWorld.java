c35b2ff0089767912654f01caf2088e7607e3bc9
chatchanok.wsa <chatchanok.wongsamang@allianz.com>
2020-08-03 15:54:38 +0700

    TC01 => create_new_buffer_should_full

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,8 +4,10 @@ public class HelloWorld {
         return "Hello World";
     }
 
-    public String sayHi(String Grace) {
-        return "Hello" + " " + Grace ;
+    public String sayHi(String name) {
+        String firstLetter = name.substring(0,1).toUpperCase();
+        String remainLetters = name.substring(1);
+        return "Hello " + firstLetter + remainLetters ;
     }
 
 }
---
+++ src/test/java/CircularBufferTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircularBufferTest {
+
+    @Test
+    public void create_new_buffer_should_empty(){
+        CircularBuffer cb = new CircularBuffer();
+        boolean result = cb.isEmpty();
+        assertTrue(result);
+    }
+
+    @Test
+    public void create_new_buffer_with_default_size_should_10(){
+        CircularBuffer cb = new CircularBuffer();
+        for (int i=0; i<10; i++) {
+            cb.writeData("A"+i);
+        }
+        boolean result = cb.isFull();
+        assertTrue(result);
+    }
+
+    @Test
+    public void write_A_B_to_buffer_should_read_A_B() {
+        CircularBuffer cb = new CircularBuffer();
+        cb.writeData("A");
+        cb.writeData("B");
+        assertEquals("A",cb.readData());
+        assertEquals("B",cb.readData());
+    }
+
+    @Test
+    public void create_new_buffer_should_full(){
+        CircularBuffer cb = new CircularBuffer();
+        boolean result = cb.isFull();
+        assertTrue(!result);
+    }
+
+}
